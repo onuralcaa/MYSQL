51481b4b4f12c1d8e460c87c4503d14dcd5658af
Onur ALACA <onuralcaa@gmail.com>
2024-04-01 23:33:05 +0300

    Table create

--- 001_1_CREATE_DATABASE.sql
+++ 001_1_CREATE_DATABASE.sql
@@ -1,2 +1,7 @@
-create database if not exists okul2;
-use okul; 
+CREATE TABLE personel(
+personel_id int NOT NULL,
+soyad varchar(255) NOT NULL,
+isim varchar(255),
+adres varchar(255),
+sehir varchar(255),
+UNIQUE (personel_id));
