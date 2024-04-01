6c930c23a062558853301c97be1388f7d538a2c7
Onur ALACA <onuralcaa@gmail.com>
2024-04-01 23:36:50 +0300

    Default command

--- 001_1_CREATE_DATABASE.sql
+++ 001_1_CREATE_DATABASE.sql
@@ -1,4 +1,4 @@
-CREATE TABLE ogrenciler (
-ogr_no int NOT NULL AUTO_INCREMENT,
-ad char (25),
-UNIQUE(ogr_no)); 
+CREATE TABLE ogrenciler2(
+ogr_no int NOT NULL,
+ad nchar (10),
+sehir nchar(50) DEFAULT 'TOKAT');
