b289834e29ce7b0d4456b209bbf420b7282d6a65
Onur ALACA <onuralcaa@gmail.com>
2024-04-09 01:20:36 +0300

    ALTER primary key

--- 014_1_ALTER.sql
+++ 014_1_ALTER.sql
@@ -1,6 +1,7 @@
 ALTER TABLE ogrenciler2 ADD dogum_tarihi date NOT NULL;
 ALTER TABLE ogrenciler2 ADD UNIQUE (ogr_no);
 ALTER TABLE ogrenciler2 ADD CONSTRAINT uc_de UNIQUE (ogr_no, ad);
+ALTER TABLE ogrenciler2 ADD PRIMARY KEY (ogr_no);
 
 
 
