f46691ed8e5a135e8d85af76f66e960118d69edb
Onur ALACA <onuralcaa@gmail.com>
2024-04-09 01:55:39 +0300

    fix

---
+++ 017_1_INSERT_EX_DML.sql
@@ -0,0 +1,9 @@
+create database ders;
+use ders;
+ create table personel(ad varchar(20),soyad varchar(20),mail varchar(50),d_tarihi date,telefon varchar(20));
+
+INSERT INTO personel VALUES ('Ali','Murat','amurat@posta.com','1968-03-02','3561231212');
+
+INSERT INTO personel VALUES ('Mustafa','Kuş','mkus@posta.com','1970-04-04','3563211453');
+
+INSERT INTO personel VALUES ('Ayşe','Demir','ademir@mail.com','1980-12-20','3563211453');
